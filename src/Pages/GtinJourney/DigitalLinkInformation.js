0144fefcb39f4cceba8dcee92f84f59b21361d08
fawadshah95 <fawad.shah@enviovista.com>
2023-10-06 21:53:57 +0500

    Commit

--- src/Pages/GtinJourney/DigitalLinkInformation.js
+++ src/Pages/GtinJourney/DigitalLinkInformation.js
@@ -10,6 +10,7 @@ import packagingCompositionIcon from "../../Images/packaging.jpeg";
 import electronicLeafletsIcon from "../../Images/electronicLeafLets.jpeg";
 import { SnackbarContext } from '../../Contexts/SnackbarContext';
 import gs1logo from "../../Images/gs1.png";
+import { FaAngleRight, FaAngleDown } from 'react-icons/fa';
 import { CurrentUserContext } from "../../Contexts/CurrentUserContext";
 import DataTable from '../../Components/Datatable/Datatable';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -29,6 +30,13 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 const DigitalLinkInformation = ({ gtinData }) => {
   const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [subOpen, setSubOpen] = useState(false);
+  const [thirdOpen, setThirdOpen] = useState(false);
+  const [fourthOpen, setFourthOpen] = useState(false);
+  const [fifthOpen, setFifthOpen] = useState(false);
+  const [sixthOpen, setSixthOpen] = useState(false);
+  const [hsCode, setHsCode] = useState('');
   const [safetyInformation, setSafetyInformation] = useState([]);
   const [recipe, setRecipe] = useState([]);
   const [productContent, setProductContent] = useState([]);
@@ -43,6 +51,36 @@ const DigitalLinkInformation = ({ gtinData }) => {
   const handleDelete = (id) => {
     console.log(id);
   }
+  // HsCode toggle Button
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  const toggleSubOpen = (e) => {
+    e.stopPropagation(); // Prevent parent from closing when a sub-item is clicked
+    setSubOpen(!subOpen);
+  };
+
+  const toggleThirdOpen = (e) => {
+    e.stopPropagation(); // Prevent parent from closing when a sub-item is clicked
+    setThirdOpen(!thirdOpen);
+  };
+
+    const toggleFourthOpen = (e) => {
+    e.stopPropagation(); // Prevent parent from closing when a sub-item is clicked
+    setFourthOpen(!fourthOpen);
+    }
+
+    const toggleFifthOpen = (e) => {
+    e.stopPropagation(); // Prevent parent from closing when a sub-item is clicked
+    setFifthOpen(!fifthOpen);
+    }
+
+    const toggleSixthOpen = (e) => {
+    e.stopPropagation(); // Prevent parent from closing when a sub-item is clicked
+    setSixthOpen(!sixthOpen);
+    }
+
 
   const [selectedOption, setSelectedOption] = useState("Safety Information");
   const { openSnackbar } = useContext(SnackbarContext);
@@ -770,6 +808,244 @@ const DigitalLinkInformation = ({ gtinData }) => {
               </div>
           </div>
         );
+
+
+        case "hsCode":
+        return (
+          <div className='h-44 w-full mt-3 border-2 border-dashed overflow-y-auto'>
+              <ul>
+                <li className=''>
+                  <a
+                    href="#"
+                    onClick={toggleOpen}
+                    className={`flex items-center px-4 hover:bg-secondary-100 focus:text-primary active:text-primary ${open ? 'text-primary' : ''}`}
+                  >
+                    {open ? (
+                      <FaAngleDown />
+                    ) : (
+                      <FaAngleRight />
+                    )}
+                    Segment 73000000 Kitchenware and Tableware
+                  </a>
+                  <ul className={`ml-6 ${open ? 'block' : 'hidden'}`}>
+                    <li className="px-2 hover:bg-secondary-100">Second-one</li>
+                    <li className="px-2 hover:bg-secondary-100">Second-two</li>
+                    <li>
+                      <a
+                        href="#"
+                        onClick={toggleSubOpen}
+                        className={`flex items-center px-2 hover:bg-secondary-100 focus:text-primary active:text-primary ${subOpen ? 'text-primary' : ''}`}
+                      >
+                        {subOpen ? (
+                          <FaAngleDown />
+                        ) : (
+                          <FaAngleRight />
+                        )}
+                        Family 73050000 Tableware
+                      </a>
+                      <ul className={`ml-6 ${subOpen ? 'block' : 'hidden'}`}>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleThirdOpen}
+                            className={`flex items-center px-4 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {thirdOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                            Class 73050000 Tableware (Disposable)
+                          </a>
+                          <ul className={`ml-10 ${thirdOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleFourthOpen}
+                            className={`flex items-center px-10 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {fourthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                            Brick 10007262 Flatware (Disposable)
+                          </a>
+                          <ul className={`ml-16 ${fourthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleFifthOpen}
+                            className={`flex items-center px-14 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {fifthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                                Attribute 10007262 Flatware Types of Disposable Flatware
+                          </a>
+                          <ul className={`ml-20 ${fifthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleSixthOpen}
+                            className={`flex items-center px-20 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {sixthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                                Attribute value 30011729 SPOON (DISPOSABLE)
+                          </a>
+                          <ul className={`ml-24 ${sixthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+          </div>
+        );
+
+
+        case "Gpc":
+        return (
+          <div className='h-44 w-full mt-3 border-2 border-dashed overflow-y-auto'>
+              <ul>
+                <li className=''>
+                  <a
+                    href="#"
+                    onClick={toggleOpen}
+                    className={`flex items-center px-4 hover:bg-secondary-100 focus:text-primary active:text-primary ${open ? 'text-primary' : ''}`}
+                  >
+                    {open ? (
+                      <FaAngleDown />
+                    ) : (
+                      <FaAngleRight />
+                    )}
+                    Segment 73000000 Kitchenware and Tableware
+                  </a>
+                  <ul className={`ml-6 ${open ? 'block' : 'hidden'}`}>
+                    <li className="px-2 hover:bg-secondary-100">Second-one</li>
+                    <li className="px-2 hover:bg-secondary-100">Second-two</li>
+                    <li>
+                      <a
+                        href="#"
+                        onClick={toggleSubOpen}
+                        className={`flex items-center px-2 hover:bg-secondary-100 focus:text-primary active:text-primary ${subOpen ? 'text-primary' : ''}`}
+                      >
+                        {subOpen ? (
+                          <FaAngleDown />
+                        ) : (
+                          <FaAngleRight />
+                        )}
+                        Family 73050000 Tableware
+                      </a>
+                      <ul className={`ml-6 ${subOpen ? 'block' : 'hidden'}`}>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleThirdOpen}
+                            className={`flex items-center px-4 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {thirdOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                            Class 73050000 Tableware (Disposable)
+                          </a>
+                          <ul className={`ml-10 ${thirdOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleFourthOpen}
+                            className={`flex items-center px-10 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {fourthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                            Brick 10007262 Flatware (Disposable)
+                          </a>
+                          <ul className={`ml-16 ${fourthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleFifthOpen}
+                            className={`flex items-center px-14 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {fifthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                                Attribute 10007262 Flatware Types of Disposable Flatware
+                          </a>
+                          <ul className={`ml-20 ${fifthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            onClick={toggleSixthOpen}
+                            className={`flex items-center px-20 hover:bg-secondary-100 focus:text-primary active:text-primary ${thirdOpen ? 'text-primary' : ''}`}
+                          >
+                            {sixthOpen ? (
+                              <FaAngleDown />
+                            ) : (
+                              <FaAngleRight />
+                            )}
+                                Attribute value 30011729 SPOON (DISPOSABLE)
+                          </a>
+                          <ul className={`ml-24 ${sixthOpen ? 'block' : 'hidden'}`}>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-one</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-two</li>
+                            <li className="px-2 hover:bg-secondary-100">Fourth-three</li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+          </div>
+        );
       // Add more cases for other options
       default:
         return null;
@@ -855,7 +1131,7 @@ const DigitalLinkInformation = ({ gtinData }) => {
               Product Contents
             </span>
             <span
-              className={`bg-[#E60023] py-2 flex justify-center rounded-md text-white items-center gap-2 cursor-pointer ${selectedOption === "ProductLocationofOrigin"
+              className={`bg-[#E60023] py-2 flex justify-start px-1 rounded-md text-white items-center gap-2 cursor-pointer ${selectedOption === "ProductLocationofOrigin"
                 ? "bg-yellow-500"
                 : ""
                 }`}
